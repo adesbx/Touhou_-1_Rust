020e4da5de0481932ab47c69f6b0cd11d70f573f
p2006393 <arthur.desbiaux@etu.univ-lyon1.fr>
2026-03-17 15:13:54 +0100

    Init bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::app::App;
+use bevy::prelude::*;
+
 fn main() {
+    App::new()
+        .add_systems(Startup, hello_wolrd)
+        .run();
+}
+
+fn hello_wolrd() {
     println!("Hello, world!");
 }
