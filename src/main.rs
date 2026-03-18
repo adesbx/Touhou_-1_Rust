b371350f6eefe8c5b92250b8e9a818dcad19c6ae
p2006393 <arthur.desbiaux@etu.univ-lyon1.fr>
2026-03-18 18:02:19 +0100

    Can now shoot projectile with speed + basic sprite

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
 const PLAYER_SPEED: f32 = 200.0;
-const PLAYER_SIZE: f32 = 24.0; // taille du sprite du joueur
+const PLAYER_SIZE: f32 = 32.0; // taille du sprite du joueur
+const PROJECTILE_SPEED: f32 = 400.0;
+const PROJECTILE_SIZE: f32 = 16.0; // taille du sprite projectile
 const ZOOM_FACTOR: f32 = 0.5; // zoom caméra
 
 fn main() {
@@ -12,6 +14,9 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(Update, move_player)
         .add_systems(Update, confine_player_movement)
+        .add_systems(Update, shoot_projectile)
+        .add_systems(Update, move_projectile)
+        .add_systems(Update, confine_projectile_movement)
         .run();
 }
 
@@ -24,7 +29,7 @@ fn setup(mut commands: Commands, asset_serv: Res<AssetServer>) {
         }),
     )); 
 
-    let texture = asset_serv.load("characters/square.png");
+    let texture = asset_serv.load("characters/character.png");
 
     commands.spawn((
         Sprite::from_image(texture),
@@ -95,6 +100,57 @@ fn confine_player_movement(
 
     player_transform.translation = translation;
 }
+
+fn shoot_projectile(
+    mut commands: Commands,
+    asset_serv: Res<AssetServer>,
+    player_transform: Single<&mut Transform, With<Player>>, 
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+    if keyboard.pressed(KeyCode::KeyK) {
+        let texture = asset_serv.load("projectiles/projectile.png");
+        commands.spawn((
+            Sprite::from_image(texture),
+            Transform::from_xyz(
+                player_transform.translation.x,
+                player_transform.translation.y+10.0,
+                player_transform.translation.z
+            ),
+            Projectile,
+        ));
+    }
+}
+
+fn move_projectile(
+    time:  Res<Time>,
+    mut projectile_query: Query<&mut Transform, With<Projectile>>, 
+) {
+    for mut transform in &mut projectile_query {
+        let mut direction: Vec2 = Vec2::ZERO;
+        direction.y += 1.;
+        transform.translation.y += direction.y * time.delta_secs() * PROJECTILE_SPEED;
+    }
+}
+
+fn confine_projectile_movement(    
+    mut commands: Commands,
+    projectile_query: Query<(Entity, &Transform), With<Projectile>>,
+    window: Single<&Window, With<PrimaryWindow>>
+) {
+    let half_projectile_size: f32 = (PROJECTILE_SIZE / 2.0) * ZOOM_FACTOR;
+    let half_height: f32 = (window.height() / 2.0) * ZOOM_FACTOR;
+
+    let y_max = half_height - half_projectile_size;
+
+    for (entity, transform) in &projectile_query {
+        if transform.translation.y > y_max {
+            commands.entity(entity).despawn();
+        }
+    }
+}
  
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct Projectile;
