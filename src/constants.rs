3ab1495e36d44c2b9c714696808168a72fc31345
Desbiaux Arthur <arthur.desbiaux@etu.univ-lyon1.fr>
2026-05-05 15:47:26 +0200

    change power up

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub const INVINCIBILITY_TIME: f32 = 3.0;
+// joueur
 pub const PLAYER_DAMAGE: f32 = 10.0;
 pub const PLAYER_HP: f32 = 3.0;
 pub const PLAYER_SPEED: f32 = 200.0;
@@ -6,17 +6,24 @@ pub const PLAYER_SIZE: f32 = 32.0; // taille du sprite du joueur
 pub const PROJECTILE_SPEED: f32 = 400.0;
 pub const PROJECTILE_SIZE: f32 = 16.0; // taille du sprite projectile
 pub const FIRE_PROJECTILE_SIZE: f32 = 20.0; // taille du sprite projectile
-pub const ANGEL_HP: f32 = 100.0; 
+pub const INVINCIBILITY_TIME: f32 = 3.0;
+
+// ennemi
 pub const ANGEL_SIZE: f32 = 32.0; // taille du sprite ange
 pub const CHERUB_SIZE: f32 = 22.0;
 pub const CROSS_PROJECTILE_SPEED: f32 = 100.0;
 pub const CROSS_PROJECTILE_SIZE: f32 = 18.0; // taille du sprite projectile
+
+// boss
 pub const BOSS_SIZE: f32 = 32.0; // taille du sprite boss
 pub const BOSS_HP: f32 = 1000.0;
 pub const BOSS_SPEED: f32 = 300.0;
 pub const BOSS_VORTEX_SPEED: f32 = 200.0;
-pub const POWER_UP_SIZE: f32 = 14.0;
-pub const BOMB_SIZE: f32 = 24.0;
 
+// item
+pub const POWER_UP_SIZE: f32 = 20.0; // taille du sprite power up
+pub const BOMB_SIZE: f32 = 24.0; // taille du sprite bombe
+
+// jeu
 pub const GAME_WIDTH: f32 = 384.0;
 pub const GAME_HEIGHT: f32 = 448.0;
